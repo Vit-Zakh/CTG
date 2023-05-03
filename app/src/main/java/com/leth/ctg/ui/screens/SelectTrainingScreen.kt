a4dfba3d3165212d5bb2b08fc3a45e765d11f024
vit_zakharenko <vit.zakharenko@gmail.com>
2023-05-03 22:09:35 +0300

    basic navigation setup

---
+++ app/src/main/java/com/leth/ctg/ui/screens/SelectTrainingScreen.kt
@@ -0,0 +1,13 @@
+package com.leth.ctg.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SelectTrainingScreen() {
+    Text(
+        text = "SelectTrainingScreen",
+        modifier = Modifier
+    )
+}
