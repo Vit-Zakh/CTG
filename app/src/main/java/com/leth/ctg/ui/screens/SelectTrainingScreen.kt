08896a31a0f902912815708000526cdb18534f32
vit_zakharenko <vit.zakharenko@gmail.com>
2023-05-04 23:12:55 +0300

    navigation extended

--- app/src/main/java/com/leth/ctg/ui/screens/SelectTrainingScreen.kt
+++ app/src/main/java/com/leth/ctg/ui/screens/SelectTrainingScreen.kt
@@ -1,13 +1,39 @@
 package com.leth.ctg.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.leth.ctg.ui.navigation.AppDestinations
+import com.leth.ctg.ui.navigation.AppDestinations.getTrainingScreenPath
+import com.leth.ctg.utils.TrainingTypes
 
 @Composable
-fun SelectTrainingScreen() {
-    Text(
-        text = "SelectTrainingScreen",
+fun SelectTrainingScreen(navigation: NavController) {
+    Column(
         modifier = Modifier
-    )
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { navigation.navigate(getTrainingScreenPath(TrainingTypes.TYPE_ONE.title)) }) {
+            Text(text = TrainingTypes.TYPE_ONE.title)
+        }
+        Button(onClick = { navigation.navigate(getTrainingScreenPath(TrainingTypes.TYPE_TWO.title)) }) {
+            Text(text = TrainingTypes.TYPE_TWO.title)
+        }
+        Button(onClick = { navigation.navigate(getTrainingScreenPath(TrainingTypes.TYPE_THREE.title)) }) {
+            Text(text = TrainingTypes.TYPE_THREE.title)
+        }
+        Button(onClick = { navigation.navigate(getTrainingScreenPath(TrainingTypes.TYPE_CROSSFIT.title)) }) {
+            Text(text = TrainingTypes.TYPE_CROSSFIT.title)
+        }
+    }
 }
