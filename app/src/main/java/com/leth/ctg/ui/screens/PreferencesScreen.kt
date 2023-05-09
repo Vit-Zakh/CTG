6468611b2c0b6243396ec19a024af1833808a71d
vit_zakharenko <vit.zakharenko@gmail.com>
2023-05-09 23:28:16 +0300

    continue working on preferences screen

--- app/src/main/java/com/leth/ctg/ui/screens/PreferencesScreen.kt
+++ app/src/main/java/com/leth/ctg/ui/screens/PreferencesScreen.kt
@@ -5,24 +5,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.Navigator
 import androidx.navigation.compose.rememberNavController
 import com.leth.ctg.domain.models.TrainingSetupModel
 import com.leth.ctg.ui.theme.CTGTheme
@@ -61,14 +56,12 @@ fun PreferencesScreen(
     )
     Column(
         modifier = modifier
-            .fillMaxWidth()
-            .fillMaxHeight(),
+            .fillMaxSize(),
     ) {
         Text(text = "Preferences")
         LazyColumn(
-            modifier = modifier
-                .fillMaxWidth()
-                .wrapContentHeight(),
+            modifier = Modifier
+                .fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(16.dp),
             contentPadding = PaddingValues(horizontal = 8.dp)
         ) {
--- app/src/main/java/com/leth/ctg/ui/views/TrainingSetupItem.kt
+++ app/src/main/java/com/leth/ctg/ui/views/TrainingSetupItem.kt
@@ -1,35 +1,40 @@
 package com.leth.ctg.ui.views
 
-import android.graphics.drawable.shapes.OvalShape
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.leth.ctg.Greeting
+import coil.compose.AsyncImage
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.leth.ctg.R
 import com.leth.ctg.domain.models.TrainingSetupModel
 import com.leth.ctg.ui.theme.CTGTheme
 import com.leth.ctg.utils.TrainingTag
@@ -40,37 +45,82 @@ fun TrainingSetupItem(
     training: TrainingSetupModel,
     onTagClick: (TrainingTag) -> Unit,
 ) {
+
+    val isChecked = remember { mutableStateOf(false) }
+    val selectedTags = remember { mutableStateOf(training.tags) }
+
     Column(
         modifier = Modifier
-//            .aspectRatio(1f)
+            .wrapContentHeight()
+            .fillMaxWidth()
             .clip(RoundedCornerShape(16.dp))
             .background(color = Color.LightGray),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = training.title,
-            modifier = Modifier.wrapContentWidth()
-        )
-        LazyHorizontalGrid(
-            rows = GridCells.Fixed(2),
+        Box(
+            modifier = Modifier
+                .wrapContentWidth()
+                .wrapContentHeight(),
+        ) {
+            Icon(
+                imageVector = Icons.Default.Close, contentDescription = "", modifier = Modifier
+                    .padding(8.dp)
+                    .wrapContentWidth()
+                    .align(Alignment.TopEnd)
+            )
+            AsyncImage(
+                model = R.drawable.test_image_ctg,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth(),
+            )
+            Text(
+                text = training.title,
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .align(Alignment.BottomStart)
+            )
+        }
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(56.dp)
                 .background(color = Color.Blue),
-            userScrollEnabled = false,
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            verticalArrangement = Arrangement.Top,
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            items(TrainingTag.values()) {
-                Text(
-                    text = it.name,
-                    modifier = Modifier
-                        .wrapContentHeight()
-                        .background(shape = CircleShape, color = Color.Green)
-                        .padding(horizontal = 8.dp)
-                        .clickable { onTagClick.invoke(it) }
-                )
+            LazyHorizontalGrid(
+                rows = GridCells.Fixed(2),
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .height(56.dp),
+                userScrollEnabled = false,
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalArrangement = Arrangement.Top,
+            ) {
+                items(TrainingTag.values()) {
+                    Text(
+                        text = it.name,
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .background(shape = CircleShape, color =
+                            if (selectedTags.value.contains(it))Color.Green else Color.DarkGray
+                            )
+                            .padding(horizontal = 8.dp)
+                            .clickable {
+                                onTagClick.invoke(it)
+                                selectedTags.value
+                                    .toMutableList()
+                                    .also { list ->
+                                        if (list.contains(it)) list.remove(it) else list.add(it)
+                                        selectedTags.value = list
+                                    }
+                            }
+                    )
+                }
             }
+            Checkbox(
+                checked = isChecked.value,
+                onCheckedChange = { isChecked.value = !isChecked.value },
+            )
         }
     }
 }
@@ -80,7 +130,6 @@ fun TrainingSetupItem(
 fun TrainingSetupItemPreview() {
     CTGTheme {
         TrainingSetupItem(
-//            modifier = Modifier,
             training = TrainingSetupModel(
                 id = "test_id",
                 title = "Test Title",
